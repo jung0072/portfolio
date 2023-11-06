6131a3345a036caea4ed7ca5797bf1182e0950d3
jung0072 <jung0072@algonquinlive.com>
2023-11-06 06:13:07 -0500

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,81 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Minki Jung",
+		email: "qmsoqm2@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/jung0072",
+		linkedin: "https://linkedin.com/minkijung",
+		stackoverflow: "https://stackoverflow.com/",
+	},
+
+	homepage: {
+		title: "Machine Learning Engineer with bachelor's degree in Physics",
+		description:
+			"I am a Machine Learning Engineer with a bachelor's degree in Physics. I have a strong background in mathematics and statistics, and I am passionate about using my skills to solve real-world problems. I am currently working as a Machine Learning Engineer at a startup company in Seoul, South Korea. I am also a member of the Machine Learning team at the company, where I am responsible for developing and deploying machine learning models for various projects.",
+	},
+
+	about: {
+		title: "Hi, I'm Minki Jung.",
+		description:
+			"I'm a Machine Learning Engineer with a bachelor's degree in Physics. I have a strong background in mathematics and statistics, and I am passionate about using my skills to solve real-world problems. I am currently working as a Machine Learning Engineer at a startup company in Seoul, South Korea. I am also a member of the Machine Learning team at the company, where I am responsible for developing and deploying machine learning models for various projects.",
+	},
+
+	articles: {
+		title: "Articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming and product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
